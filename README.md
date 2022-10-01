8ede11262b64971940a0373b19e73483b0853381
andress15s <carlos2214104@correo.uis.edu.co>
2022-10-01 11:19:21 -0500

    programa final

---
+++ README.md
@@ -0,0 +1,5 @@
+# Carro_POO
+Hacer un programa que simule una situación con un carro.
+
+# Diagrama 
+![Diagrama](carro.png)
---
+++ ejecutable/Test.java
@@ -0,0 +1,18 @@
+package ejecutable;
+
+import controlador.Controlador;
+import vista.VentanaPrincipal;
+import modelo.Carro;
+
+
+public class Test 
+{
+    public static void main(String[] args)
+    {
+
+        VentanaPrincipal miVentana = new VentanaPrincipal();
+        Carro miCarro = null;
+        Controlador miControlador = new Controlador(miVentana, miCarro);
+
+    }
+}
